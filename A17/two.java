3020c3565521ea88d92dddb89d7de86130958007
SandaliBhadane <sandalibhadane20@gmail.com>
2025-11-22 14:25:03 +0530

    Add files via upload

---
+++ A17/two.java
@@ -0,0 +1,38 @@
+
+////////////////////////////////////////////////////////////
+// 
+//  File name :     One.java
+//  Description :   To calculate Sum of Digits of a given number
+//  Author :        Sandali sunil Bhadane
+//  Date :          30/10/2025
+//
+////////////////////////////////////////////////////////////
+
+class Logic
+{
+    int CalculateSum(int iNo)
+    {
+        int iSum = 0, iDigit = 0;
+
+        while (iNo != 0)
+        {
+            iDigit = iNo % 10;
+            iSum = iSum + iDigit;
+            iNo = iNo / 10;         
+        }
+
+        return iSum;
+    }
+}
+
+class one
+{
+    public static void main(String A[])
+    {
+        Logic lobj = new Logic();
+        int iValue = 12345;
+        int iRet = lobj.CalculateSum(iValue);
+
+        System.out.println("Sum of digits of " + iValue + " is: " + iRet);
+    }
+}
