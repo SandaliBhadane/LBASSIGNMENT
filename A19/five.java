95067e340e062f8d799ab7e10af07ee7897ba64e
SandaliBhadane <sandalibhadane20@gmail.com>
2025-11-22 14:27:38 +0530

    Add files via upload

---
+++ A19/five.java
@@ -0,0 +1,44 @@
+
+////////////////////////////////////////////////////////////
+// 
+//  File name :     Five.java
+//  Description :   To calculate the power of a number using 
+//                  for loop
+//  Author :        Sandali Bhadane
+//  Date :          30/10/2025
+//
+////////////////////////////////////////////////////////////
+
+class Logic
+{
+    int CalculatePower(int iBase, int iExponent)
+    {
+        int iResult = 1;
+        int iCnt = 0;
+        
+        if (iExponent < 0)
+        {
+            iExponent = -iExponent;
+        }
+        for (iCnt = 1; iCnt <= iExponent; iCnt++)
+        {
+            iResult = iResult * iBase;
+        }
+
+        return iResult;
+    }
+}
+
+class five
+{
+    public static void main(String A[])
+    {
+        Logic lobj = new Logic();
+        int base = 2;
+        int exponent = 5;
+
+        int iRet = lobj.CalculatePower(base, exponent);
+
+        System.out.println(base + " raised to the power " + exponent + " is: " + iRet);
+    }
+}
