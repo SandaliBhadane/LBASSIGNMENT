16d33fa4b4c37d1f547e788e1a5d91ffbe386f85
SandaliBhadane <sandalibhadane20@gmail.com>
2025-11-22 14:29:53 +0530

    Add files via upload

---
+++ A21/three.java
@@ -0,0 +1,38 @@
+
+////////////////////////////////////////////////////////////
+//
+//  File name :     Three.java
+//  Description :   To display all factors of a given number
+//  Author :        Sandali Bhadane
+//  Date :          30/10/2025
+//
+////////////////////////////////////////////////////////////
+
+class Logic
+{
+    void DisplayFactors(int iNo)
+    {
+        int iCnt = 0;
+
+        System.out.println("Factors of " + iNo + " are:");
+
+        for (iCnt = 1; iCnt <= iNo; iCnt++)
+        {
+            if (iNo % iCnt == 0)
+            {
+                System.out.println(iCnt);
+            }
+        }
+    }
+}
+
+class three
+{
+    public static void main(String A[])
+    {
+        Logic lobj = new Logic();
+        int iValue = 12;
+
+        lobj.DisplayFactors(iValue);
+    }
+}
